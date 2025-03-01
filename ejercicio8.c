115fc47c287bf70263b35b6ddca151c9031c0dc8
Cebollino945 <angelhergonzalez0@gmail.com>
2025-02-28 21:04:33 -0600

    ejercicio listo

--- ejercicio8.c
+++ ejercicio8.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 
 void intercambiar(int *a, int *b) {
+    int cambiar = *a;
+    *a = *b;
+    *b = cambiar;
     // Implementar intercambio aquí
 }
 
 int main() {
     int arr[] = {5, 10};
+    printf("Antes del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
     // Código para llamar a la función y mostrar los valores intercambiados
+    intercambiar(&arr[0], &arr[1]);
+
+    printf("Despues del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
     return 0;
 }
